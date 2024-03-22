69bdef79eeaaf7d50f2fd1f1e7964c7352e591c0
Maksim Morozov <max2morozov@gmail.com>
2024-03-23 00:09:49 +0300

    Commit (Homework-1) write code channel.py and ID YouTube

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,26 @@
+import os
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self._channel_id = channel_id
+        self.channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.url = "https://www.youtube.com/channel/UCZ_B-KkMbNAsax9H6ohFTqw"
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return print(channel)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.}"
+
+    @property
+    def channel_id(self):
+        return self._channel_id
