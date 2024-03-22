7dc3fe67370f2fe94332ea4ea4bc8f508f8b48a4
Maksim Morozov <max2morozov@gmail.com>
2024-03-23 00:32:41 +0300

    (fix) Commit (Homework-1) channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,16 +1,17 @@
+import json
 import os
+from googleapiclient.discovery import build
 
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self._channel_id = channel_id
-        self.channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        self.title = self.channel_data['items'][0]['snippet']['title']
-        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.channel_id = channel_id
         self.url = "https://www.youtube.com/channel/UCZ_B-KkMbNAsax9H6ohFTqw"
 
     def print_info(self) -> None:
@@ -18,9 +19,7 @@ class Channel:
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         return print(channel)
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.}"
 
-    @property
-    def channel_id(self):
-        return self._channel_id
+    #@property
+    #def channel_id(self):
+        #return self._channel_id
